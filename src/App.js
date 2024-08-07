465289967aaed3afcae02e9532c731cda56eff22
harish <harishlodh444@gmail.com>
2024-08-07 20:43:12 +0530

    added alert

--- src/App.js
+++ src/App.js
@@ -7,76 +7,101 @@ import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import ListGroup from "react-bootstrap/ListGroup";
+import { ChakraProvider, Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/react';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        // Setting up state
-        this.state = {
-            userInput: "",
-            list: [],
-        };
-    }
+    // Setting up state
+    this.state = {
+      userInput: "",
+      list: [],
+      showAlert: false,
+      alertMessage: "", 
+      alertStatus: 'success', // New state for alert type (success, error, etc.)
+    };
+  }
 
-    // Set a user input value
-    updateInput(value) {
-        this.setState({
-            userInput: value,
-        });
-    }
+  // Set a user input value
+  updateInput(value) {
+    this.setState({
+      userInput: value,
+    });
+  }
 
-    // Add item if user input in not empty
-    addItem() {
-        if (this.state.userInput !== "") {
-            const userInput = {
-                // Add a random id which is used to delete
-                id: Math.random(),
+  // Function to handle showing the alert
+  handleAlert(message, status) {
+    this.setState({
+      showAlert: true,
+      alertMessage: message,
+      alertStatus: status,
+    });
 
-                // Add a user value to list
-                value: this.state.userInput,
-            };
+    // Hide the alert after 3 seconds
+    setTimeout(() => this.setState({ showAlert: false }), 3000);
+  }
 
-            // Update list
-            const list = [...this.state.list];
-            list.push(userInput);
+  // Add item if user input is not empty
+  addItem() {
+    if (this.state.userInput !== "") {
+      const userInput = {
+        // Add a random id which is used to delete
+        id: Math.random(),
+        // Add a user value to list
+        value: this.state.userInput,
+      };
 
-            // reset state
-            this.setState({
-                list,
-                userInput: "",
-            });
-        }
-    }
+      // Update list
+      const list = [...this.state.list];
+      list.push(userInput);
 
-    // Function to delete item from list use id to delete
-    deleteItem(key) {
-        const list = [...this.state.list];
-
-        // Filter values and leave value which we need to delete
-        const updateList = list.filter((item) => item.id !== key);
-
-        // Update list in state
-        this.setState({
-            list: updateList,
-        });
-    }
-
-    editItem = (index) => {
-      const todos = [...this.state.list];
-      const editedTodo = prompt('Edit the todo:');
-      if (editedTodo !== null && editedTodo.trim() !== '') {
-        let updatedTodos = [...todos]
-        updatedTodos[index].value= editedTodo
-        this.setState({
-          list: updatedTodos,
+      // Reset state
+      this.setState({
+        list,
+        userInput: "",
       });
-      }
-    }
 
-    render() {
-      return (
-        <Container>
+      // Show success alert
+      this.handleAlert("The item has been added to your list.", "success");
+    } else {
+      // Show error alert
+      this.handleAlert("Please enter notes or etc", "error");
+    }
+  }
+
+  // Function to delete item from list use id to delete
+  deleteItem(key) {
+    const list = [...this.state.list];
+    // Filter values and leave value which we need to delete
+    const updateList = list.filter((item) => item.id !== key);
+
+    // Update list in state
+    this.setState({
+      list: updateList,
+    });
+  }
+
+  editItem = (index) => {
+    const todos = [...this.state.list];
+    const editedTodo = prompt('Edit the todo:');
+    if (editedTodo !== null && editedTodo.trim() !== '') {
+      let updatedTodos = [...todos];
+      updatedTodos[index].value = editedTodo;
+      this.setState({
+        list: updatedTodos,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <ChakraProvider>
+        <Container style={{
+    
+    boxSizing: 'border-box' // Ensure padding and border are included in the width and height
+  }}
+         >
           <Row
             style={{
               display: "flex",
@@ -89,7 +114,7 @@ class App extends Component {
           >
             TODO LIST
           </Row>
-      
+
           <hr />
           <Row>
             <Col md={{ span: 5, offset: 4 }}>
@@ -105,7 +130,8 @@ class App extends Component {
                 <InputGroup>
                   <Button
                     style={{
-                      backgroundColor:"green"}}
+                      backgroundColor: "green"
+                    }}
                     className="mt-2"
                     onClick={() => this.addItem()}
                   >
@@ -128,15 +154,16 @@ class App extends Component {
                         style={{
                           display: "flex",
                           justifyContent: "space-between",
-                          backgroundColor: "#E2EAF4", 
+                          backgroundColor: "#E2EAF4",
                         }}
                       >
                         {item.value}
                         <span>
                           <Button
-                            style={{ marginRight: "10px",
-                              backgroundColor:"red"
-                             }}
+                            style={{
+                              marginRight: "10px",
+                              backgroundColor: "red"
+                            }}
                             variant="light"
                             onClick={() => this.deleteItem(item.id)}
                           >
@@ -144,10 +171,10 @@ class App extends Component {
                           </Button>
                           <Button
                             style={{
-                              backgroundColor:"blue"}}
+                              backgroundColor: "blue"
+                            }}
                             variant="light"
                             onClick={() => this.editItem(index)}
-                          
                           >
                             Edit
                           </Button>
@@ -159,8 +186,31 @@ class App extends Component {
               </ListGroup>
             </Col>
           </Row>
-        </Container>
-      );
-    }}
+          <footer className="footer">
     
+          {this.state.showAlert && (
+  <Alert 
+   
+        status={this.state.alertStatus}
+        style={{ 
+          position: 'fixed', 
+          bottom: '20px', 
+          left: '50%', 
+          transform: 'translateX(-50%)', 
+          zIndex: 1000, 
+          width: '500px', // Adjust the width as needed
+        }}
+      >
+    <AlertIcon />
+    <AlertTitle>{this.state.alertStatus === 'error' ? "Error!" : "Success!"}</AlertTitle>
+    <AlertDescription>{this.state.alertMessage}</AlertDescription>
+  </Alert>
+)}
+</footer>
+        </Container>
+      </ChakraProvider>
+    );
+  }
+}
+
 export default App;
